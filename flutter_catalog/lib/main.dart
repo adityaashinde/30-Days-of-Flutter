38bda32b219b3c883be0c5d79763e2dae04b9cfd
Aditya Shinde <adityashinde4580@gmail.com>
2023-09-28 23:15:24 +0530

    Day 3 : routes, Theme, Text

--- flutter_catalog/lib/main.dart
+++ flutter_catalog/lib/main.dart
@@ -1,21 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/pages/login_page.dart';
+import 'pages/home_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-            child: Container(
-          child: Text("30 Days Of Flutter"),
-        )),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
       ),
+      routes: {
+        "/": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
     );
   }
 }
