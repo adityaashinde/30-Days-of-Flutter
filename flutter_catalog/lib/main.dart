24d9bb3c572a5cd2e61a8c03ba384138da41e825
Aditya Shinde <adityashinde4580@gmail.com>
2023-09-29 10:32:58 +0530

    Day 4 : adding Image, Elevated Button, Google Fonts

--- flutter_catalog/lib/main.dart
+++ flutter_catalog/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/pages/login_page.dart';
 import 'pages/home_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,13 +12,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
       routes: {
-        "/": (context) => HomePage(),
-        "/login": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/": (context) => LoginPage(),
       },
     );
   }
