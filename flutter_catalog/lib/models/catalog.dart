8fb551084bb89bed08fb77b4725ff13377b11a11
Aditya Shinde <adityashinde4580@gmail.com>
2023-10-15 19:16:58 +0530

    Day 16 : GridView | GridTile

--- flutter_catalog/lib/models/catalog.dart
+++ flutter_catalog/lib/models/catalog.dart
@@ -7,7 +7,7 @@ class CatalogModel {
         id: 1,
         name: "iPhone 12 Pro",
         desc: "Apple iPhone 12th generation",
-        price: 49999,
+        price: 999,
         color: "#33505a",
         image:
             "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-12-pro-blue-hero?wid=940&hei=1112&fmt=png-alpha&qlt=80&.v=1604021661000"),
--- flutter_catalog/lib/pages/home_page.dart
+++ flutter_catalog/lib/pages/home_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
-import 'package:flutter_catalog/widgets/item_widget.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -13,8 +12,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  bool isLoading = true;
-
   @override
   void initState() {
     super.initState();
@@ -22,16 +19,16 @@ class _HomePageState extends State<HomePage> {
   }
 
   loadData() async {
-    var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
-    var decodedData = jsonDecode(catalogJson);
+    await Future.delayed(const Duration(seconds: 5));
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
     var productsData = decodedData["products"];
     CatalogModel.items = List.from(productsData)
         .map<Item>((item) => Item.fromMap(item))
         .toList();
 
-    setState(() {
-      isLoading = false; // Data is loaded, set isLoading false
-    });
+    setState(() {});
   }
 
   @override
@@ -40,19 +37,46 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: const Text("Catalog App"),
       ),
-      body: isLoading
-          ? const Center(
-              child: CircularProgressIndicator(),
-            )
-          : Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: ListView.builder(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        // ignore: unnecessary_null_comparison
+        child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
+            ? GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 16,
+                  crossAxisSpacing: 16,
+                ),
+                itemBuilder: (context, index) {
+                  final item = CatalogModel.items[index];
+                  return Card(
+                      clipBehavior: Clip.antiAlias,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      child: GridTile(
+                        header: Container(
+                          // ignore: sort_child_properties_last
+                          child: Text(item.name),
+                          padding: const EdgeInsets.all(12),
+                          decoration: const BoxDecoration(
+                            color: Colors.deepPurple,
+                          ),
+                        ),
+                        // ignore: sort_child_properties_last
+                        child: Image.network(item.image),
+                        footer: Text(
+                          item.price.toString(),
+                        ),
+                      ));
+                },
                 itemCount: CatalogModel.items.length,
-                itemBuilder: (context, index) => ItemWidget(
-                  item: CatalogModel.items[index],
+              )
+            : const Center(
+                child: CircularProgressIndicator(
+                  color: Colors.deepPurple,
                 ),
               ),
-            ),
+      ),
       drawer: const MyDrawer(),
     );
   }
